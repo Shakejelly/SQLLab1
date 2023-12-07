f76389dadbb0b4007b901809b61cf69451c82165
Shakejelly <fredrich93@gmail.com>
2023-12-07 09:32:48 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp3
+    
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string conntectionString = @"Data Source = (localdb)\.; Initial Catalog = SchoolLabb1; Integrated Security = True; Pooling = False";
+            using (SqlConnection connection = new SqlConnection(conntectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand()) 
+                { 
+                }
+            }
+        }
+    }
+}
